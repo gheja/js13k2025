c7affef9f7e5ad6ab55f3045cc56df564dcf4552
Gabor Heja <gheja@ubuntu1.w830g8.git.stdio.hu>
2025-09-04 01:13:07 +0200

    consts.ts: now using const instead of var

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
 // the following will be replaced with true by the build process in prod builds
-var IS_PROD_BUILD = false
+const IS_PROD_BUILD = false
 
 // used in physics calculation
-var TARGET_TICK_INTERVAL_MS = 1000 / 60
-var GRAVITY = 11 // pixel/sec/sec
-var FALL_GRAVITY = 22 // pixel/sec/sec
-var PLAYER_JUMP_SPEED = 15
-var PLAYER_JUMP_SPEED_EXTRA_MULTIPLIER = 0.25 // the jump height varies with the running speed. it will be multiplied with this number and added to the jump speed
-var PLAYER_DRAG_MULTIPLIER = 0.94
-var PLAYER_DRAG_CLAMP_TO_ZERO = 0.66
+const TARGET_TICK_INTERVAL_MS = 1000 / 60
+const GRAVITY = 11 // pixel/sec/sec
+const FALL_GRAVITY = 22 // pixel/sec/sec
+const PLAYER_JUMP_SPEED = 15
+const PLAYER_JUMP_SPEED_EXTRA_MULTIPLIER = 0.25 // the jump height varies with the running speed. it will be multiplied with this number and added to the jump speed
+const PLAYER_DRAG_MULTIPLIER = 0.94
+const PLAYER_DRAG_CLAMP_TO_ZERO = 0.66
