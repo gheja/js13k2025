ae0bf76372d88c1f7d4bf9474d8dc01655a715b3
Gabor Heja <gheja@ubuntu1.w830g8.git.stdio.hu>
2025-09-07 18:07:28 +0200

    Moved GfxSprite

---
+++ src/gfx_sprite.ts
@@ -0,0 +1,54 @@
+class GfxSprite {
+    originalWidth: number
+    originalHeight: number
+    svg: SvgInHtml
+
+    constructor(data) {
+        this.originalWidth = data[0]
+        this.originalHeight = data[1]
+        var arr: Array<number>
+
+        var str = ""
+        str += `<svg width="${this.originalWidth}" height="${this.originalHeight}" viewBox="0 0 ${this.originalWidth} ${this.originalHeight}" version="1.1" xmlns="http://www.w3.org/2000/svg">`
+
+        for (var j=0; j<data[2].length; j++)
+        {
+            arr = data[2][j]
+
+            var s = "M "
+            // the first element is the style index
+            for (var i=1; i<arr.length; i+=2){
+                s += (arr[i] / 100 * this.originalWidth) + "," + (arr[i+1] / 100 * this.originalHeight) + " "
+            }
+            // this closes the shape, we don't need the final move back to the first point
+            s += "Z"
+
+            str += '<path style="' + SVG_STYLES[arr[0]] + '" d="' + s + '"/>'
+        }
+
+        str += '</svg>'
+
+        var parser = new DOMParser()
+        this.svg = parser.parseFromString(str, "image/svg+xml").documentElement as SvgInHtml
+        _gfx_root.appendChild(this.svg)
+
+        this.moveAway()
+    }
+
+    moveTo(x: number, y: number) {
+        this.svg.style.left = (x * _gfx_scale + _gfx_pad_x) + "px"
+        this.svg.style.top = (y * _gfx_scale + _gfx_pad_y) + "px"
+        // TODO: maybe we should only change the width and height when the window was rescaled
+        // TODO: now the sprite position and scale is only updated when moveTo() is called
+        this.svg.style.width = (this.originalWidth * _gfx_scale) + "px"
+        this.svg.style.height = (this.originalHeight * _gfx_scale) + "px"
+    }
+
+    moveAway() {
+        this.moveTo(10000, 10000)
+    }
+
+    cleanup() {
+        this.svg.parentNode.removeChild(this.svg)
+    }
+}
